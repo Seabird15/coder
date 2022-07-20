8b52ea519a865135cb1b6cde3d729e1864157a17
Daniela Paz <danielalizama.a@gmail.com>
2022-07-20 19:07:29 -0400

    commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let submit = prompt("Ingrese nombres de animales")
+    while (submit != "Salir") {
+        alert ("Ingresaste " + submit)
+        submit = prompt ("ingrese otro nombre, escriba Salir para terminar")
+    }
+
